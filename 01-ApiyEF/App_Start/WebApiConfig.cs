53b6b41c3036eab60c400182302a02c4da9b1fdb
Piris-Zazo <Piris-Zazo@DESKTOP-3OKJPBK>
2017-01-24 19:17:37 +0100

    Add project files.

---
+++ 01-ApiyEF/App_Start/WebApiConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace _01_ApiyEF
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            #region Devolucion de datos en JSON
+
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.Objects;
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            #endregion
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
